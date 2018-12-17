cd1eb5d770c9026b65278608518b86220a5993eb
BryhanR <brarodriguezm@hotmail.com>
2018-12-16 22:53:11 -0600

    Setup functional project
    Uploaded the root project structure, it's the really basic poject structure, it will need to be touched some tmes to get the best project structure.

---
+++ educ_backend/educ_backend/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url 
+from django.contrib import admin 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
+from django.views.generic import TemplateView 
+
+urlpatterns = [
+    url(r'^$',
+        TemplateView.as_view(template_name='index.html'), name='uHome'
+    ), 
+    url(r'^admin/', admin.site.urls),
+] 
+
+urlpatterns += staticfiles_urlpatterns()
